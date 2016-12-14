06b75d6ec6533ed8ad655135672b79453ee41bb0
dlewis2017 <dlewfaith@gmail.com>
2016-12-14 02:37:44 -0500

    login basic html template set

---
+++ javascripts/index.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyAVONv-JNWkMiuN3hBV_u2Mx0J-fpM3_dQ",
+    authDomain: "schurz-ica.firebaseapp.com",
+    databaseURL: "https://schurz-ica.firebaseio.com",
+    storageBucket: "schurz-ica.appspot.com",
+    messagingSenderId: "795121797573"
+};
+firebase.initializeApp(config);
+
