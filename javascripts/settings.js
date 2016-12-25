d93a2f3f586bd29c2d7ae131628e22c1dd9ff40d
dlewis2017 <dlewfaith@gmail.com>
2016-12-25 01:27:00 -0500

    working on edit of user profile settings page

--- javascripts/settings.js
+++ javascripts/settings.js
@@ -8,12 +8,21 @@ var config = {
 firebase.initializeApp(config);
 
 window.onload = function(){
-    //get Team Name before page loads
+    //get Team Name before page loads to pass back to home if need be 
     var tmp = location.search.split('team=')[1];
     var team_name = decodeURI(tmp);
 
     var a = document.getElementById("homeLink");
     a.href = 'home.html?team='+team_name;
+    //event handlers
+    document.getElementById('picBtn').addEventListener('click', updatePic, false);
 
+    //load information
 }
 
+function updatePic(){
+    //add url to 
+    var url = document.getElementById("picURL");
+    
+
+}
